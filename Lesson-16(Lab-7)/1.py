a72931a1c22bc006090590270c7b2d2b825b9f7a
Bayashat <baiashat2002@gmail.com>
2022-04-11 15:30:27 +0600

    Miccky problem fixed

--- Lesson-16(Lab-7)/1.py
+++ Lesson-16(Lab-7)/1.py
@@ -1,55 +1,25 @@
-import pygame as pg
-from datetime import datetime
-pg.init()
+import pygame,datetime
+pygame.init()
 
-WIDTH, HEIGHT = 1000,800
-FPS = 45
+screen=pygame.display.set_mode((800,600))
+bg=pygame.transform.scale(pygame.image.load('mickeybg.jpeg'),(800,600))   # өлшемін өзгерту
+sec=pygame.transform.smoothscale(pygame.image.load('mickeym.png'),(800,600))  # тегіс түрде масштабын өзгертеді
+min=pygame.transform.smoothscale(pygame.image.load('mickeyh.png'),(800,600))
+cl=pygame.time.Clock()
 
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption('clock')
+def r_center(s,image, angle, x, y): 
+    r_image = pygame.transform.rotate(image, angle) 
+    n_rect = r_image.get_rect(center=image.get_rect(center=(x, y)).center) 
+    s.blit(r_image,n_rect)
 
-clock = pg.time.Clock()
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-def blitRotate(surf,image,topleft,angle):
-    rotated_image = pg.transform.rotate(image,angle)
-    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
-
-    surf.blit(rotated_image,new_rect)
-    
-background = pg.image.load("background.jpg").convert()
-background = pg.transform.smoothscale(background, (1200,1000))
-
-h = pg.image.load("min.jpg").convert()
-h = pg.transform.smoothscale(h, (200,200))
-
-h2 = pg.image.load("sec.jpg").convert()
-
-h2 = pg.transform.smoothscale(h2, (200,200))
-
-def time(time):
-    return 360-time*6
-
-font = pg.font.SysFont("Times New Roman",40)
-
-running = True
-while running:
-    clock.tick(1)
-    #keys = pg.key.get_pressed()
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-    screen.blit(background,(0,0))
-    t=datetime.now()
-    angle_sec = time(t.second)
-    angle_min = time(t.minute)
-
-    blitRotate(screen,h,(500,400),angle_min)
-    blitRotate(screen,h2,(500,400),angle_sec)
-
-    pg.display.update()
+run=True
+while run:
+    screen.blit(bg,(0,0))
+    time=datetime.datetime.now()
+    cl.tick(30)
+    r_center(screen,sec,-time.second*6,400,300)   # Минус жаздық, чтобы оң бағытта айналу үшін, *6 өйткені 1с  = 6 градус
+    r_center(screen,min,-time.minute*6-42,400,300)  # *6 өйткені 1минут  = 6 градус,  -42 өйткені суретіміз бастапқыда 42 ге градусқа солға қисайып тур
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+    pygame.display.update() 
